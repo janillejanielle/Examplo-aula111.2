5d6b4a17a03e9d1403d13bdab542bef725522b4c
janillejanielle <janille.janielle2@gmail.com>
2022-09-16 17:45:49 -0300

    Primeiro commit

---
+++ src/test/java/com/aula1112/Examplo/ExamploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula1112.Examplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
